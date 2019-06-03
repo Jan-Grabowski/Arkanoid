b455cfa1894a24233fccc9bfc2045c07ea40a06b
Jan-Grabowski <jan.grabowski1999@gmail.com>
2019-06-03 19:25:34 +0200

    Instrukcja 4 dokończona piłak na naciśnięcie lewego przycisku myszy odrywa sie od platformy

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,25 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
+    [SerializeField] bool hasStarted = false;
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
+    [SerializeField] Rigidbody2D rb;
     Vector2 paddletoBallVector;
     
     private void Start()
     {
         paddletoBallVector = transform.position - paddle1.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        rb.simulated = false;
     }
 
-    // Update is called once per frame
+
     private void Update()
     {
-        LockBalltoPaddle();
+        if (!hasStarted)
+        {
+            LockBalltoPaddle();
+            LaunchonMouseclick();
+        }
     }
+
+    private void LaunchonMouseclick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rb.simulated = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
     private void LockBalltoPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddletoBallVector;
     }
 }
+
